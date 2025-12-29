beae178b9d876687bfee7ac4bd90e32f79b0385f
Nischay <nischaythapar2002@gmail.com>
2025-12-29 19:08:11 +0530

    Disabling mongock for CI (will be used later in the prod with actual Mongo and not the localhost)

--- springboot-mongodb/src/test/java/com/programming/learner/springboot_mongodb/SpringbootMongodbApplicationTests.java
+++ springboot-mongodb/src/test/java/com/programming/learner/springboot_mongodb/SpringbootMongodbApplicationTests.java
@@ -3,9 +3,11 @@ package com.programming.learner.springboot_mongodb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringbootMongodbApplicationTests {
 
 	@Test
