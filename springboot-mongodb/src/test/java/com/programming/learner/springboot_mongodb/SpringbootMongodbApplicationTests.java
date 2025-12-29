7ef320649464fc0b89bfbe0c9a77d244e275e142
Nischay <nischaythapar2002@gmail.com>
2025-12-29 19:22:08 +0530

    More changes to make the pipeline green

--- springboot-mongodb/src/test/java/com/programming/learner/springboot_mongodb/SpringbootMongodbApplicationTests.java
+++ springboot-mongodb/src/test/java/com/programming/learner/springboot_mongodb/SpringbootMongodbApplicationTests.java
@@ -4,10 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 class SpringbootMongodbApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer =
+			new MongoDBContainer("mongo:7.0");
+
+	@DynamicPropertySource
+	static void overrideProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
